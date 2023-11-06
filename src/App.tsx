513c05809c9c57af60b7bd4acf0244d1c423f7b3
junny97 <pcohad12@gmail.com>
2023-11-06 20:23:02 +0900

    Style: GlobalStyle 설정 #2

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-
+import { useState } from 'react';
+import styled from 'styled-components';
+import { GlobalStyle } from './styles/GlobalStyle';
 function App() {
-  return <div>APP</div>;
+  return <GlobalStyle />;
 }
-
 export default App;

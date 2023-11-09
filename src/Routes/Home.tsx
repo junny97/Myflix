e941358f8d083054fc778963eaf42e35a9bc8edb
junny97 <pcohad12@gmail.com>
2023-11-09 17:32:29 +0900

    Feat: 영화 Detail Modal 기능 구현#5

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,10 +1,11 @@
+import { useState } from 'react';
+import { useNavigate, useMatch, PathMatch } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import styled from 'styled-components';
 import { getMovies } from '../utils/api';
 import { IGetMoviesResult } from '../interface';
 import { makeImagePath } from '../utils/utilsFn';
 import { motion, AnimatePresence } from 'framer-motion';
-import { useState } from 'react';
 import useWindowDimensions from '../hook/useWindowDimensions';
 
 function Home() {
@@ -16,6 +17,9 @@ function Home() {
   const width = useWindowDimensions();
   const [index, setIndex] = useState(0);
   const [leaving, setLeaving] = useState(false);
+  const navigate = useNavigate();
+  const moviePathMatch: PathMatch<string> | null = useMatch('/movies/:movieId');
+
   const increaseIndex = () => {
     if (data) {
       if (leaving) return;
@@ -27,6 +31,23 @@ function Home() {
   };
 
   const toggleLeaving = () => setLeaving((prev) => !prev);
+
+  const handleBoxClicked = (movieId: number) => {
+    navigate(`/movies/${movieId}`);
+  };
+
+  const hanldeOverlayClicked = () => {
+    navigate(-1);
+  };
+
+  const clickMovieBox =
+    moviePathMatch?.params.movieId &&
+    data?.results.find(
+      (movie) => movie.id === Number(moviePathMatch.params.movieId)
+    );
+
+  // console.log(moviePathMatch);
+  console.log(clickMovieBox);
 
   return (
     <Wrapper>
@@ -55,11 +76,13 @@ function Home() {
                   .slice(offset * index, offset * index + offset)
                   .map((movie) => (
                     <MovieBox
+                      layoutId={String(movie.id)} //toString
                       key={movie.id}
                       whileHover='hover'
                       initial='normal'
                       variants={boxVariants}
                       transition={{ type: 'tween' }}
+                      onClick={() => handleBoxClicked(movie.id)}
                       $bgPhoto={makeImagePath(movie.backdrop_path, 'w500')}
                     >
                       <MovieInfo variants={infoVariants}>
@@ -70,6 +93,35 @@ function Home() {
               </Row>
             </AnimatePresence>
           </Slider>
+          <AnimatePresence>
+            {moviePathMatch ? (
+              <>
+                <ModalOverlay
+                  variants={overlayVariants}
+                  initial='hidden'
+                  animate='visible'
+                  exit='exit'
+                  onClick={hanldeOverlayClicked}
+                />
+                <ModalBox layoutId={moviePathMatch.params.movieId}>
+                  {clickMovieBox && (
+                    <>
+                      <MovieCover
+                        style={{
+                          backgroundImage: `linear-gradient(to top, black, transparent), url(${makeImagePath(
+                            clickMovieBox.backdrop_path,
+                            'w500'
+                          )})`,
+                        }}
+                      />
+                      <MovieTitle>{clickMovieBox.title}</MovieTitle>
+                      <MovieOverview>{clickMovieBox.overview}</MovieOverview>
+                    </>
+                  )}
+                </ModalBox>
+              </>
+            ) : null}
+          </AnimatePresence>
         </>
       )}
     </Wrapper>
@@ -130,6 +182,7 @@ const MovieBox = styled(motion.div)<{ $bgPhoto: string }>`
   background-position: center center;
   height: 200px;
   font-size: 66px;
+  cursor: pointer;
   &:first-child {
     transform-origin: center left;
   }
@@ -150,6 +203,51 @@ const MovieInfo = styled(motion.div)`
     text-align: center;
     font-size: 18px;
   }
+`;
+
+const ModalOverlay = styled(motion.div)`
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  opacity: 0;
+`;
+
+const ModalBox = styled(motion.div)`
+  position: fixed;
+  width: 50vw;
+  height: 70vh;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+  border-radius: 15px;
+  overflow: hidden;
+  background-color: ${(props) => props.theme.black.lighter};
+`;
+
+const MovieCover = styled.div`
+  width: 100%;
+  background-size: cover;
+  background-position: center center;
+  height: 400px;
+`;
+
+const MovieTitle = styled.h3`
+  color: ${(props) => props.theme.white.lighter};
+  padding: 20px;
+  font-size: 46px;
+  position: relative;
+  top: -80px;
+`;
+
+const MovieOverview = styled.p`
+  padding: 20px;
+  position: relative;
+  top: -80px;
+  color: ${(props) => props.theme.white.lighter};
 `;
 
 const boxVariants = {
@@ -177,4 +275,10 @@ const infoVariants = {
       type: 'tween',
     },
   },
+};
+
+const overlayVariants = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 0.7 },
+  exit: { opacity: 0 },
 };

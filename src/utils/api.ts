d10aa7b968a3011a8e7205d41ed7d761441e1a72
junny97 <pcohad12@gmail.com>
2023-11-10 16:35:37 +0900

    Feat: 검색 요청 받아오는 api 설정하기#

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -16,8 +16,24 @@ const API_KEY = 'd90f98a8d4a4304af3042e12308b63b5';
 const BASE_URL = 'https://api.themoviedb.org/3';
 
 export const getMovies = async () => {
-  const response = await axios.get(
-    `${BASE_URL}/movie/now_playing?api_key=${API_KEY}`
-  );
-  return response.data;
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/movie/now_playing?language=ko&api_key=${API_KEY}`
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getSearch = async (search: string) => {
+  try {
+    const encodedSearch = encodeURIComponent(search); // 검색어를 URL-safe한 형태로 인코딩
+    const response = await axios.get(
+      `${BASE_URL}/search/multi?api_key=${API_KEY}&query=${encodedSearch}&include_adult=false&language=ko&page=1`
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
 };
